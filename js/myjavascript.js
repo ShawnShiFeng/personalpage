6187310f1f36b02d10f6369408e774b077a527f1
Shawn <shawnsfeng@gmail.com>
2016-06-07 05:37:46 +0000

    first stage completed

--- js/myjavascript.js
+++ js/myjavascript.js
@@ -1,6 +1,16 @@
-function submit_validation(i){
-   if(document.form1.input_fullname.value=="")
+function submission_validation() {
+   if (document.form1.input_fullname1.value == "" || document.form1.input_email1.value == "") 
    {
-      window.write("please type your fullname");
+      document.getElementById("footer-error-message").textContent = "Please fill out both \"Full name\" and \"Email address\" sections before submit, thank you.";
+      if (document.form1.input_fullname1.value == "")
+         document.form1.input_fullname1.focus();
+      else
+         document.form1.input_email1.focus();
+      return false;
+   }
+   else if (document.form1.input_fullname1.value !="" && document.form1.input_email1.value !="")
+   {
+      window.alert("Your massage to Shawn has been sent, thank you.");
+      return true;
    }
 }
